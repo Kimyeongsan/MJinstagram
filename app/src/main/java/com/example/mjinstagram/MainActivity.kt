d3c2afdcdd926e5535de5bf50fd006497faca381
Kimyeongsan <apua34@gmail.com>
2021-01-20 23:55:55 +0900

    Add_Photo(FireStore) 사진 권한 add

--- app/src/main/java/com/example/mjinstagram/MainActivity.kt
+++ app/src/main/java/com/example/mjinstagram/MainActivity.kt
@@ -1,39 +1,95 @@
 package com.example.mjinstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import java.util.jar.Manifest
+import androidx.core.content.ContextCompat
+import com.example.mjinstagram.navigation.account.AccountFragment
+import com.example.mjinstagram.navigation.home.HomeFragment
+import com.example.mjinstagram.navigation.notice.NotificationsFragment
+import com.example.mjinstagram.navigation.photo.PhotoActivity
+import com.example.mjinstagram.navigation.search.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        nav_view.setOnNavigationItemSelectedListener(this)
+        nav_view.selectedItemId = R.id.navigation_home
+
         var actionBar: ActionBar?
         actionBar = supportActionBar
         actionBar?.hide()
 
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.EARD_EXTERNAL_STORAGE),1)
-
-        naviation()
+        // 앨범 접근 권한 요청
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 
-    fun naviation() {
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_home -> {
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_photo, R.id.navigation_notifications, R.id.navigation_account))
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                val homeFragment = HomeFragment()
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, homeFragment)
+                        .commit()
+                return true
+            }
+            R.id.navigation_search -> {
+                val searchFragment = SearchFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment, searchFragment)
+                        .commit()
+                return true
+            }
+            R.id.navigation_photo -> {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, PhotoActivity::class.java))
+                } else {
+                    Toast.makeText(this, "스토리지 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
+                }
+                return true
+            }
+            R.id.navigation_notifications -> {
+                val notificationsFragment = NotificationsFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.nav_host_fragment, notificationsFragment)
+                        .commit()
+                return true
+            }
+            R.id.navigation_account -> {
+                val accountFragment = AccountFragment()
+                supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, accountFragment)
+                        .commit()
+                return true
+            }
+        }
+        return false
     }
+
+
+//    fun naviation() {
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_search, R.id.navigation_photo, R.id.navigation_notifications, R.id.navigation_account))
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+//    }
 }
--- app/src/main/java/com/example/mjinstagram/navigation/photo/PhotoActivity.kt
+++ app/src/main/java/com/example/mjinstagram/navigation/photo/PhotoActivity.kt
@@ -4,11 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mjinstagram.R
 import com.example.mjinstagram.data.PhotoDTO
 import com.google.firebase.auth.FirebaseAuth
@@ -18,8 +16,7 @@ import kotlinx.android.synthetic.main.acitivity_add_photo.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class PhotoFragment : Fragment() {
-
+class PhotoActivity : AppCompatActivity() {
     val PICK_IMAGE_FROM_ALBUM = 0
 
     var photoUri: Uri? = null
@@ -28,12 +25,9 @@ class PhotoFragment : Fragment() {
     var firestore: FirebaseFirestore? = null
     private var auth: FirebaseAuth? = null
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_photo, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_add_photo)
 
         // Firebase storage
         storage = FirebaseStorage.getInstance()
@@ -56,7 +50,6 @@ class PhotoFragment : Fragment() {
             contentUpload()
         }
 
-        return root
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -72,50 +65,55 @@ class PhotoFragment : Fragment() {
             }
 
             else{
-                activity?.finish()
+                finish()
             }
 
         }
     }
 
-
     fun contentUpload(){
+        progress_bar.visibility = View.VISIBLE
 
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val imageFileName = "JPEG_" + timeStamp + "_.png"
         val storageRef = storage?.reference?.child("images")?.child(imageFileName)
         storageRef?.putFile(photoUri!!)?.addOnSuccessListener{ taskSnapshot ->
+            progress_bar.visibility = View.GONE
 
-            Toast.makeText(activity, getString(R.string.upload_success),
+            Toast.makeText(this, getString(R.string.upload_success),
                     Toast.LENGTH_SHORT).show()
 
-            val uri = taskSnapshot.downloadUrl
-            //디비에 바인딩 할 위치 생성 및 컬렉션(테이블)에 데이터 집합 생성
-
-            //시간 생성
-            val contentDTO = PhotoDTO()
-
-            //이미지 주소
-            contentDTO.imageUrl = uri!!.toString()
-            //유저의 UID
-            contentDTO.uid = auth?.currentUser?.uid
-            //게시물의 설명
-            contentDTO.explain = addphoto_edit_explain.text.toString()
-            //유저의 아이디
-            contentDTO.userId = auth?.currentUser?.email
-            //게시물 업로드 시간
-            contentDTO.timestamp = System.currentTimeMillis()
-
-            //게시물을 데이터를 생성 및 엑티비티 종료
-            firestore?.collection("images")?.document()?.set(contentDTO)
-
-            activity?.setResult(Activity.RESULT_OK)
-            activity?.finish()
+//            val uri = taskSnapshot.downloadUrl
+//            //디비에 바인딩 할 위치 생성 및 컬렉션(테이블)에 데이터 집합 생성
+//
+//
+//            //시간 생성
+//            val contentDTO = PhotoDTO()
+//
+//            //이미지 주소
+//            contentDTO.imageUrl = uri!!.toString()
+//            //유저의 UID
+//            contentDTO.uid = auth?.currentUser?.uid
+//            //게시물의 설명
+//            contentDTO.explain = addphoto_edit_explain.text.toString()
+//            //유저의 아이디
+//            contentDTO.userId = auth?.currentUser?.email
+//            //게시물 업로드 시간
+//            contentDTO.timestamp = System.currentTimeMillis()
+//
+//            //게시물을 데이터를 생성 및 엑티비티 종료
+//            firestore?.collection("images")?.document()?.set(contentDTO)
+//
+//            setResult(Activity.RESULT_OK)
+//            finish()
         }
-                ?.addOnFailureListener {
-                    Toast.makeText(activity, getString(R.string.upload_fail),
-                            Toast.LENGTH_SHORT).show()
-                }
+//                ?.addOnFailureListener {
+//                    progress_bar.visibility = View.GONE
+//
+//                    Toast.makeText(this, getString(R.string.upload_fail),
+//                            Toast.LENGTH_SHORT).show()
+//                }
     }
+
 
 }
